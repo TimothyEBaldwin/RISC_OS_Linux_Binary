0f5b30d13530f4ff02c496c4af706d5600173cf7
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2018-09-07 13:36:05 +0100

    Rebuilt binary from a29492c1852fcb418a30b8084044268eccb773e0

--- Support/gen_seccomp.c
+++ Support/gen_seccomp.c
@@ -32,10 +32,20 @@
 #include <error.h>
 #include <stdbool.h>
 #include <sys/ioctl.h>
+#include <sys/prctl.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 #include <seccomp.h>
+
+static scmp_filter_ctx ctx;
+
+static void ban(int syscall, const char* message) {
+  int rc = seccomp_rule_add(ctx, SCMP_ACT_ERRNO(EPERM), SCMP_SYS(syscall), 0);
+  if (rc) error(1, -rc, message);
+}
+
+#define BAN(s) ban(SCMP_SYS(s), "Unable to create " #s " rule")
 
 int main(int argc, char **argv) {
 
@@ -56,7 +66,7 @@ int main(int argc, char **argv) {
       break;
   }
 
-  scmp_filter_ctx ctx = seccomp_init(SCMP_ACT_ALLOW);
+  ctx = seccomp_init(SCMP_ACT_ALLOW);
   if (!ctx) error(1, errno, "Unable to create libseccomp context");
 
 #ifdef __aarch64__
@@ -69,11 +79,8 @@ int main(int argc, char **argv) {
   if (rc) error(1, -rc, "Unable to create rule to block inserting into terminal input buffer");
 
   if (!allow_symlinks) {
-    rc = seccomp_rule_add(ctx, SCMP_ACT_ERRNO(EPERM), SCMP_SYS(symlink), 0);
-    if (rc) error(1, -rc, "Unable to create symlink rule");
-
-    rc = seccomp_rule_add(ctx, SCMP_ACT_ERRNO(EPERM), SCMP_SYS(symlinkat), 0);
-    if (rc) error(1, -rc, "Unable to create symlinkat rule");
+    BAN(symlink);
+    BAN(symlinkat);
   }
 
   if (!allow_unix_socket) {
@@ -82,18 +89,16 @@ int main(int argc, char **argv) {
   }
 
   if (!allow_ptrace) {
-    rc = seccomp_rule_add(ctx, SCMP_ACT_ERRNO(EPERM), SCMP_SYS(ptrace), 0);
-    if (rc) error(1, -rc, "Unable to create UNIX socket rule");
+    BAN(ptrace);
+  } else {
+    BAN(seccomp);
+    rc = seccomp_rule_add(ctx, SCMP_ACT_ERRNO(EPERM), SCMP_SYS(prctl), 1, SCMP_A0(SCMP_CMP_EQ, PR_SET_SECCOMP));
+    if (rc) error(1, -rc, "Unable to prctl(PR_SET_SECCOMP...) rule");
   }
 
-  rc = seccomp_rule_add(ctx, SCMP_ACT_ERRNO(EPERM), SCMP_SYS(keyctl), 0);
-  if (rc) error(1, -rc, "Unable to create keyctl rule");
-
-  rc = seccomp_rule_add(ctx, SCMP_ACT_ERRNO(EPERM), SCMP_SYS(request_key), 0);
-  if (rc) error(1, -rc, "Unable to create request_key rule");
-
-  rc = seccomp_rule_add(ctx, SCMP_ACT_ERRNO(EPERM), SCMP_SYS(add_key), 0);
-  if (rc) error(1, -rc, "Unable to create add_key rule");
+  BAN(keyctl);
+  BAN(request_key);
+  BAN(add_key);
 
   rc = seccomp_export_bpf(ctx, STDOUT_FILENO);
   if (rc) error(1, -rc, "Unable to load seccomp rules");
--- Support/wrapper.c
+++ Support/wrapper.c
@@ -30,34 +30,42 @@
 #define _GNU_SOURCE
 #include <errno.h>
 #include <error.h>
-#include <stdbool.h>
-#include <sys/wait.h>
-#include <sys/socket.h>
-#include <sys/prctl.h>
-#include <termios.h>
-#include <unistd.h>
-#include <signal.h>
 #include <fcntl.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/prctl.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <termios.h>
+#include <unistd.h>
 
 struct termios oldTioIn, newTioIn;
 
-static void stop_handler(int signal) {
+static void stop_handler(int s) {
   tcsetattr(0, TCSANOW, &oldTioIn);
   raise(SIGSTOP);
   tcsetattr(0, TCSANOW, &newTioIn);
 }
 
+static void fatal_handler(int s) {
+  tcsetattr(0, TCSANOW, &oldTioIn);
+  signal(s, SIG_DFL);
+  raise(s);
+}
+
 int main(int argc, char **argv) {
 
-
-  // Ignore fatal signals
-  signal(SIGINT, SIG_IGN);
-  signal(SIGTERM, SIG_IGN);
+  // Die if parent dies
+  prctl(PR_SET_PDEATHSIG, SIGTERM, 0, 0, 0);
 
   // Get terminal status, if successful reopen terminal for input.
   if (!tcgetattr(0, &oldTioIn)) {
+
+    // Standard input is a terminal, ro reopen it avoid
+    // other programs having to cope with non-blocking etc.
     int fd = open("/proc/self/fd/0", O_RDONLY);
     if (fd > 0) {
       dup2(fd, 0);
@@ -70,76 +78,88 @@ int main(int argc, char **argv) {
     newTioIn.c_cc[VMIN] = 1;
     newTioIn.c_cc[VTIME] = 0;
     newTioIn.c_lflag &= ~ICANON & ~ECHO;
+
+    // Install cleanup signal handlers
     signal(SIGTSTP, &stop_handler);
     signal(SIGTTIN, &stop_handler);
     signal(SIGTTOU, &stop_handler);
+    signal(SIGINT,  &fatal_handler);
+    signal(SIGHUP,  &fatal_handler);
+    signal(SIGTERM, &fatal_handler);
 
+    // Set new termnal state
     tcsetattr(0, TCSANOW, &newTioIn);
-
   }
 
-  int sockets[2];
-  socketpair(AF_UNIX, SOCK_SEQPACKET | SOCK_CLOEXEC, 0, sockets);
+  int status = 0;
 
-  pid_t self = getpid();
-  pid_t pid = fork();
-  if (pid < 0) {
-    return 109;
-  }
-  if (!pid) {
-    // Don't Ignore fatal signals
-    signal(SIGINT, SIG_DFL);
-    signal(SIGTERM, SIG_DFL);
+  do {
+    int sockets[2];
+    socketpair(AF_UNIX, SOCK_SEQPACKET | SOCK_CLOEXEC, 0, sockets);
 
-    prctl(PR_SET_PDEATHSIG, SIGTERM, 0, 0, 0);
-    int socket = fcntl(sockets[1], F_DUPFD, 32);
-    char s[48];
-    sprintf(s, "RISC_OS_Internet_SocketServer=%i", socket);
-    putenv(s);
+    pid_t self = getpid();
+    pid_t pid = fork();
+    if (pid < 0) {
+      tcsetattr(0, TCSANOW, &oldTioIn);
+      return 109;
+    }
+    if (!pid) {
+      prctl(PR_SET_PDEATHSIG, SIGTERM, 0, 0, 0);
+      if (getppid() != self) _exit(110);
 
-    if (getppid() != self) _exit(110);
-    execvp(argv[1], argv + 1);
-    _exit(116);
-  }
-  close(sockets[1]);
+      int socket = fcntl(sockets[1], F_DUPFD, 32);
+      char s[48];
+      sprintf(s, "RISC_OS_Internet_SocketServer=%i", socket);
+      putenv(s);
 
-  while(true) {
-    int32_t socket_args[3];
-    int s = read(sockets[0], socket_args, sizeof(socket_args));
-    if (s == 0) break;
+      lseek(9, SEEK_SET, 0);
 
-    s = -1;
-    if (socket_args[0] == AF_INET) {
-      s = socket(socket_args[0], socket_args[1], socket_args[2]);
+      execvp(argv[1], argv + 1);
+      _exit(116);
+    }
+    close(sockets[1]);
+
+    while(true) {
+      int32_t socket_args[3];
+      int s = read(sockets[0], socket_args, sizeof(socket_args));
+      if (s == 0) break;
+
+      s = -1;
+      if (socket_args[0] == AF_INET) {
+        s = socket(socket_args[0], socket_args[1], socket_args[2]);
+      }
+
+      union {
+        char buf[CMSG_SPACE(sizeof(int))];
+        struct cmsghdr align;
+      } u;
+
+      struct msghdr msg = {
+        .msg_control = u.buf,
+        .msg_controllen = sizeof(u.buf)
+      };
+      struct cmsghdr *cmsg;
+      cmsg = CMSG_FIRSTHDR(&msg);
+      cmsg->cmsg_level = SOL_SOCKET;
+      cmsg->cmsg_type = SCM_RIGHTS;
+      cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+      memcpy(CMSG_DATA(cmsg), &s, sizeof(s));
+
+      sendmsg(sockets[0], &msg, 0);
+      close(s);
     }
 
-    union {
-      char buf[CMSG_SPACE(sizeof(int))];
-      struct cmsghdr align;
-    } u;
+    close(sockets[0]);
 
-    struct msghdr msg = {
-      .msg_control = u.buf,
-      .msg_controllen = sizeof(u.buf)
-    };
-    struct cmsghdr *cmsg;
-    cmsg = CMSG_FIRSTHDR(&msg);
-    cmsg->cmsg_level = SOL_SOCKET;
-    cmsg->cmsg_type = SCM_RIGHTS;
-    cmsg->cmsg_len = CMSG_LEN(sizeof(int));
-    memcpy(CMSG_DATA(cmsg), &s, sizeof(s));
+    // Write a new line to keep output tidy.
+    if (isatty(1)) write(1, "\n", 1);
 
-    sendmsg(sockets[0], &msg, 0);
-    close(s);
-  }
-
-  // Write a new line to keep output tidy.
-  if (isatty(1)) write(1, "\n", 1);
+    int wstatus;
+    waitpid(pid, &wstatus, 0);
+    status = WEXITSTATUS(wstatus);
+  } while(status == 100);
 
   // Restore terminal
   tcsetattr(0, TCSANOW, &oldTioIn);
 
-  int wstatus;
-  waitpid(pid, &wstatus, 0);
-  return WEXITSTATUS(wstatus);
 }
