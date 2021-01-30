1b8941ac1d6b45eb121efdab00d472d25ae73860
Timothy E Baldwin <T.E.Baldwin99@members.leeds.ac.uk>
2021-01-30 23:20:00 +0000

    Rebuilt binary from bf926112ade3b6eae3d70977de6b95e46cf0f459

--- Unix/LinuxSupport/common.mk
+++ Unix/LinuxSupport/common.mk
@@ -16,8 +16,8 @@
 #
 
 HARDDISC4=$(HOME)/Downloads/HardDisc4.5.28.util
-QEMU_SRC=$(HOME)/Downloads/qemu-5.0.0.tar.xz
-RPCEMU=$(HOME)/Downloads/rpcemu-0.8.15.tar.gz
+QEMU_SRC=$(HOME)/Downloads/qemu-5.2.0.tar.xz
+RPCEMU=$(HOME)/Downloads/rpcemu-0.9.3.tar.gz
 IOMD=$(HOME)/Downloads/IOMD-Soft.5.28.zip
 
 LINUX_ROM=./RISC_OS
@@ -84,41 +84,36 @@ Built/gen_seccomp: Unix/LinuxSupport/gen_seccomp.c $(lib_depends) | Built
 Built/seccomp%: Built/gen_seccomp
 	Built/gen_seccomp $* > $@
 
-Built/rpcemu/stamp2: $(RPCEMU) Unix/LinuxSupport/rpcemu_exit.diff | Built/gen_seccomp
+Built/rpcemu/stamp3: $(RPCEMU) Unix/LinuxSupport/rpcemu.diff | Built/gen_seccomp
 	set -o pipefail
 	rm -rf Built/rpcemu*
 	mkdir -p Built/rpcemu_files
 	unpack() {
-	  echo "3f092e6000b5d50984e63768a74cdc9a40284d55c984e26df44c7d5875ced6e9 /rpcemu.tar.gz" | sha256sum -c
+	  echo "33b89e02e62b5621c625aa6d388d3a357e7ee013e74a00fcf53ef68f31d19605 /rpcemu.tar.gz" | sha256sum -c
 	  tar -zxf /rpcemu.tar.gz
-	  cd rpcemu-0.8.15
+	  cd rpcemu-0.9.3
 	  patch -p1 < /d
-	  touch stamp2
+	  touch stamp3
 	}
 	export -f unpack
-	$(sandbox_base) $(sandbox_misc) --file 8 8<'$(RPCEMU)' /rpcemu.tar.gz --ro-bind Unix/LinuxSupport/rpcemu_exit.diff /d --bind Built/rpcemu_files /r --chdir /r $(BASHF) unpack </dev/null |& cat
-	mv Built/rpcemu_files/rpcemu-0.8.15 Built/rpcemu
+	$(sandbox_base) $(sandbox_misc) --file 8 8<'$(RPCEMU)' /rpcemu.tar.gz --ro-bind Unix/LinuxSupport/rpcemu.diff /d --bind Built/rpcemu_files /r --chdir /r $(BASHF) unpack </dev/null |& cat
+	mv Built/rpcemu_files/rpcemu-0.9.3 Built/rpcemu
 
-Built/rpcemu/src/Makefile: Built/rpcemu/stamp2 | Built/gen_seccomp
-	set -o pipefail
-	configure() {
-	  if uname -m | grep -E -q 'x86|i386'; then
-	    ./configure --enable-dynarec CFLAGS="-no-pie -fno-pie"
-	  else
-	    ./configure
-	  fi
-	  touch Makefile
-	}
-	export -f configure
-	$(sandbox_base) $(sandbox_build) --bind Built/rpcemu /r --chdir /r/src $(BASHF) configure </dev/null |& cat
-
-Built/rpcemu/rpcemu: Built/rpcemu/src/Makefile | Built/gen_seccomp
+Built/rpcemu/rpcemu: Built/rpcemu/stamp3 | Built/gen_seccomp
 	set -o pipefail
 	+cp Unix/LinuxSupport/rpcemu.cfg Built/rpcemu/rpc.cfg
 	build() {
 	  touch hd4.hdf roms/ROM
-	  $(MAKE) -C src
-	  ! rm poduleroms/SyncClock,ffa
+	  (
+	    cd src/qt5
+	    if uname -m | grep -E -q 'x86|i386'; then
+	      qtchooser -run-tool=qmake -qt=5 CONFIG+=dynarec
+	    else
+	      qtchooser -run-tool=qmake -qt=5
+	    fi
+	    $(MAKE)
+	  )
+	  rm -f poduleroms/SyncClock,ffa
 	  echo H4sIAISQCFUCA2NgYDNTuO7MAAdlTAEM+Q4MDgwaNgxoQABdgOHVGoZ/DK8ZZoJlQxQ4uHQmMDGw\
 	wWSZGWgP+EUYGJb8FXD8z5jIIAgV+0CMRm5GBgYTBgYAXYSkcwABAAA= | base64 -d | gzip -d > cmos.ram
 	  touch rpcemu
@@ -126,22 +121,22 @@ Built/rpcemu/rpcemu: Built/rpcemu/src/Makefile | Built/gen_seccomp
 	export -f build
 	$(sandbox_base) $(sandbox_build) --bind Built/rpcemu /r --chdir /r $(BASHF) build </dev/null |& cat
 
-Built/qemu_stamp-v5.0.0: ${QEMU_SRC} Unix/LinuxSupport/qemu_swi.diff | Built/gen_seccomp
+Built/qemu_stamp-v5.2.0: ${QEMU_SRC} Unix/LinuxSupport/qemu_swi.diff | Built/gen_seccomp
 	set -o pipefail
 	rm -rf Built/qemu*
 	mkdir -p Built/qemu_files
 	unpack() {
-	  echo "2f13a92a0fa5c8b69ff0796b59b86b080bbb92ebad5d301a7724dd06b5e78cb6 /qemu.tar.xz" | sha256sum -c
+	  echo "cb18d889b628fbe637672b0326789d9b0e3b8027e0445b936537c78549df17bc /qemu.tar.xz" | sha256sum -c
 	  tar -Jxf /qemu.tar.xz
-	  cd qemu-5.0.0
+	  cd qemu-5.2.0
 	  patch -p1 < /d
 	}
 	export -f unpack
 	$(call sandbox_base,-s) $(sandbox_misc) --file 8 8<'${QEMU_SRC}' /qemu.tar.xz --ro-bind Unix/LinuxSupport/qemu_swi.diff /d --bind Built/qemu_files /q --chdir /q $(BASHF) unpack </dev/null |& cat
-	mv Built/qemu_files/qemu-5.0.0 Built/qemu
-	touch Built/qemu_stamp-v5.0.0
+	mv Built/qemu_files/qemu-5.2.0 Built/qemu
+	touch Built/qemu_stamp-v5.2.0
 
-Built/qemu_Makefile_stamp: Built/qemu_stamp-v5.0.0 | Built/gen_seccomp
+Built/qemu_Makefile_stamp: Built/qemu_stamp-v5.2.0 | Built/gen_seccomp
 	set -o pipefail
 	$(call sandbox_base,-s) $(sandbox_build) --bind Built/qemu /q --chdir /q ./configure --enable-attr --target-list=arm-linux-user --disable-werror </dev/null |& cat
 	touch Built/qemu_Makefile_stamp
@@ -149,9 +144,9 @@ Built/qemu_Makefile_stamp: Built/qemu_stamp-v5.0.0 | Built/gen_seccomp
 Built/qemu-arm: Built/qemu_Makefile_stamp | Built/gen_seccomp
 	set -o pipefail
 	+$(call sandbox_base,-s) $(sandbox_build) --bind Built/qemu /q --chdir /q $(MAKE) </dev/null |& cat
-	test ! -L Built/qemu/arm-linux-user
-	test ! -L Built/qemu/arm-linux-user/qemu-arm
-	ln -f Built/qemu/arm-linux-user/qemu-arm Built/qemu-arm
+	test ! -L Built/qemu/build
+	test ! -L Built/qemu/build/qemu-arm
+	ln -f Built/qemu/build/qemu-arm Built/qemu-arm
 	touch Built/qemu-arm
 
 Built/sandbox_config_sh: $(BWRAP) $(QEMU) $(lib_depends)
@@ -206,7 +201,7 @@ endif
 	mv HardDisc4_files/HardDisc4 .
 	echo Finished extracting HardDisc4
 
-Built/boot_iomd_rom: $(IOMD) | Built
+Built/boot_iomd_rom: $(IOMD) | Built/gen_seccomp
 	echo 'd1e955ff8e6dce905c455b9135391f3f1879673965fb4949c5f10716b4fe3e3e *$(IOMD)' | sha256sum -c
 	$(sandbox_base) $(sandbox_misc) --ro-bind '$(IOMD)' /iomd.zip unzip -p iomd.zip "soft/!Boot/Choices/Boot/PreDesk/!!SoftLoad/riscos" > $@
 	echo '051bd488965d43dc8d74757ee29fe72927e0f018dfe45e9c88e511496f0ec316 *$@' | sha256sum -c
@@ -221,9 +216,9 @@ $(IOMD):
 	setfattr -n user.RISC_OS.LoadExec -v 0x0091faff00000000 $@ || true
 
 ${QEMU_SRC}:
-	sh Unix/LinuxSupport/download.sh '${QEMU_SRC}' "https://download.qemu.org/qemu-5.0.0.tar.xz" "2f13a92a0fa5c8b69ff0796b59b86b080bbb92ebad5d301a7724dd06b5e78cb6"
+	sh Unix/LinuxSupport/download.sh '${QEMU_SRC}' "https://download.qemu.org/qemu-5.2.0.tar.xz" "cb18d889b628fbe637672b0326789d9b0e3b8027e0445b936537c78549df17bc"
 	setfattr -n user.RISC_OS.LoadExec -v 0x00fdffff00000000 $@ || true
 
 $(RPCEMU):
-	sh Unix/LinuxSupport/download.sh '$(RPCEMU)' "http://www.marutan.net/rpcemu/cgi/download.php?sFName=0.8.15/rpcemu-0.8.15.tar.gz" "3f092e6000b5d50984e63768a74cdc9a40284d55c984e26df44c7d5875ced6e9"
+	sh Unix/LinuxSupport/download.sh '$(RPCEMU)' "http://www.marutan.net/rpcemu/cgi/download.php?sFName=0.9.3/rpcemu-0.9.3.tar.gz" "33b89e02e62b5621c625aa6d388d3a357e7ee013e74a00fcf53ef68f31d19605"
 	setfattr -n user.RISC_OS.LoadExec -v 0x0089ffff00000000 $@ || true
